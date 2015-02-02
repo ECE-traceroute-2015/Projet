6532b77a63b615c76d3b02b10b972c540cbc589a
larbish <leproux.baptiste@gmail.com>
2015-02-02 16:10:00 +0100

    suppression code inutile
    supr code inutil

--- TP1/src/Controller/GraphController.java
+++ TP1/src/Controller/GraphController.java
@@ -36,101 +36,98 @@ public class GraphController
     {
         System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         try{
+            
             InetAddress localIP =  InetAddress.getLocalHost();
             String hostIP = localIP.getHostAddress() ;
             Node _myRoot = new Node(hostIP); 
         
     
-    Tree _myTree = new Tree(_myRoot);
-    
-    
-    Process p = null;
-
-    try {
-        String commande = "java -jar ./lib/fakeroute.jar " + adresse;
-        p = Runtime.getRuntime().exec(commande);
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
+            Tree _myTree = new Tree(_myRoot);
 
 
-    BufferedReader output = getOutput(p);
-    BufferedReader error = getError(p);
-    String ligne = "";
-    List<String> _listeIPs = new ArrayList<>();
-    String destination = "";
+            Process p = null;
 
-    try{
-        
-        if((ligne = output.readLine()) != null)
-        {
-            destination = _myTree.getIPs(ligne, destination).get(0);
-        }
-        while ((ligne = output.readLine()) != null) {
-            //System.out.println("**********************");
-            //System.out.println(ligne);
-            //System.out.println("**********************");
-                    
-       // Address destinationIP = Address.getByName(adresse);
-        //destination = destinationIP.getHostAddress();
-                    
-            _listeIPs = _myTree.getIPs(ligne,destination);
-            boolean _myBool = _myTree.addChildren(_listeIPs);
-            if(_myBool)
-            {
-                System.out.println("Children add\n");
+            try {
+                String commande = "java -jar ./lib/fakeroute.jar " + adresse;
+                p = Runtime.getRuntime().exec(commande);
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            else 
-            {
-                System.out.println("Children not add");
+
+
+            BufferedReader output = getOutput(p);
+            BufferedReader error = getError(p);
+            String ligne = "";
+            List<String> _listeIPs = new ArrayList<>();
+            String destination = "";
+
+            try{
+
+                if((ligne = output.readLine()) != null)
+                {
+                    destination = _myTree.getIPs(ligne, destination).get(0);
+                }
+                while ((ligne = output.readLine()) != null) {
+                    //System.out.println("**********************");
+                    //System.out.println(ligne);
+                    //System.out.println("**********************");
+
+               // Address destinationIP = Address.getByName(adresse);
+                //destination = destinationIP.getHostAddress();
+
+                    _listeIPs = _myTree.getIPs(ligne,destination);
+                    boolean _myBool = _myTree.addChildren(_listeIPs);
+                    if(_myBool)
+                    {
+                        System.out.println("Children add\n");
+                    }
+                    else 
+                    {
+                        System.out.println("Children not add");
+                    }
+                    _listeIPs.clear();
+
+
+
+                }
+                _listeIPs.add(destination);
+                _myTree.addChildren(_listeIPs);
+
+                while ((ligne = error.readLine()) != null) {
+                    System.out.println(ligne);
+
+                }
+
+                _myTree.displayTree();
+
+
+
+                 System.out.println("\n");
+
+                
+
+                
+
+
+
+
+                 boolean ex;
+                 ex = View.Graph.newGraph(_myTree);
+
             }
-            _listeIPs.clear();
-            
-            
-            
-        }
-        _listeIPs.add(destination);
-        _myTree.addChildren(_listeIPs);
+             catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        while ((ligne = error.readLine()) != null) {
-            System.out.println(ligne);
-
-        }
-        
-        _myTree.displayTree();
-
-
-        
-         System.out.println("\n");
-        
-        System.out.println( _myTree.getMyNodes().get(2).getAddress());
-        
-        for(int i=0; i<_myTree.getMyNodes().get(2).getChildren().size() ; i++)
-        {
-            System.out.println("mes noeuds:");
-            _myTree.getMyNodes().get(2).getChildren().get(i).displayNode();
-        }
-        
-        
-
-   
-         boolean ex;
-         ex = View.Graph.newGraph(_myTree);
-
-    }
-     catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    }catch(UnknownHostException uhe)
-        {
-            uhe.printStackTrace();
-        }
+            }catch(UnknownHostException uhe)
+                {
+                    uhe.printStackTrace();
+                }
 
 
 
-        return true; 
+                return true; 
     }
     
     public static boolean traceGraphAuto() throws UnknownHostException
