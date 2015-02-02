20459db209c725575e2b31378e71cbcf0946c14c
lgrondin <lionegrondin2@gmail.com>
2015-02-02 13:43:55 +0100

    implementation du gui
    ree

--- TP1/src/Controller/GraphController.java
+++ TP1/src/Controller/GraphController.java
@@ -3,28 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Controller;
-
 
 import Model.Node;
 import Model.Tree;
-import View.Fenetre;
-import View.Graph;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.InputStreamReader;
 import org.graphstream.graph.implementations.SingleGraph;
 
 /**
  *
- * @author Larbish
+ * @author lionelgrondin
  */
-public class MainController {
-
-    
+public class GraphController 
+{
     private static BufferedReader getOutput(Process p) {   
 
         return new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -33,20 +28,9 @@ public class MainController {
     private static BufferedReader getError(Process p) {
         return new BufferedReader(new InputStreamReader(p.getErrorStream()));
     }
-
-   
-    public static void main(String[] args) {
-
-
-        
-    // Window creation
-    Fenetre f = new Fenetre();
-      
-
-    
-
-
-    System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
+    public boolean traceGraph(String adresse)
+    {
+        System.setProperty("gs.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 
     Node _myRoot = new Node("123");
     
@@ -56,7 +40,7 @@ public class MainController {
     Process p = null;
 
     try {
-        String commande = "java -jar ./lib/fakeroute.jar www.ece.fr";
+        String commande = "java -jar ./lib/fakeroute.jar "+adresse;
         p = Runtime.getRuntime().exec(commande);
 
     } catch (IOException e) {
@@ -124,7 +108,8 @@ public class MainController {
 
 
 
-}
-  
-}
 
+        return true; 
+    }
+    
+}
